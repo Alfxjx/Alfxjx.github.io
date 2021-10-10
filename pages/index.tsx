421e027845e1f7d4d9cd98745a09ac06a52b9869
xujx <xjxtju@163.com>
2021-10-10 21:13:16 +0800

    fix: full page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -120,7 +120,7 @@ const SelfIntro = ({ newPost }) => {
 
 const FullPage = styled.div`
 	width: 100vw;
-	height: 100vh;
+	min-height: 100vh;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
