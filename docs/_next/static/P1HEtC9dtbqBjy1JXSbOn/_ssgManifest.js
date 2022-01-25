fa3639fe278d322f8067744acc68aaed36530150
xujx <xjxtju@163.com>
2022-01-25 13:47:08 +0800

    build

--- docs/_next/static/P1HEtC9dtbqBjy1JXSbOn/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Farchive","\u002Ftech","\u002Fabout","\u002Flife","\u002Flife\u002F[slug]","\u002Ftech\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
