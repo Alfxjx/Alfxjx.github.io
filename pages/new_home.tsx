853be55e30a7e9e9f459c4ec1a4dae7a868f1ea1
xujx <xjxtju@163.com>
2022-01-25 13:28:03 +0800

    fix: change yarn registry

---
+++ pages/new_home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components'
+
+export default function NewHome() {
+  return (
+    <NewHomeWrapper></NewHomeWrapper>
+  )
+}
+
+const NewHomeWrapper = styled.div``;
