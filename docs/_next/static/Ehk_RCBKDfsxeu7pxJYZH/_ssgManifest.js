ca3378561a268f8cecac4e4c47d2ee6842b1f910
xujx <xjxtju@163.com>
2022-02-01 21:38:11 +0800

    build: blog deploy

--- docs/_next/static/Ehk_RCBKDfsxeu7pxJYZH/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fhome-till-2022-01-27l","\u002Farchive","\u002Fabout","\u002Flife","\u002Ftech","\u002Ftech\u002F[slug]","\u002Flife\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
