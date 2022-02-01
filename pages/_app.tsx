b34ea7a27dd7fb39132997a872b6fb1004d92472
xujx <xjxtju@163.com>
2022-02-01 21:56:18 +0800

    fix: add vercel.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,19 @@ import "../styles/base.css";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "../components/theme/theme";
 import { useDarkMode } from "../components/theme/Toggle";
-import { StyledContainer } from '../components/Toast/index';
+import { StyledContainer } from "../components/Toast/index";
 import { createContext } from "react";
-const FontStyles = createGlobalStyle`
-@font-face {
-	font-family: "GreenHome";
-	src: url("/fonts/GreenHome.ttf");
-	font-style: normal;
-	font-weight: 400;
-}
-.my-main-font{
-	font-family: "GreenHome";
-}
 
+const FontStyles = createGlobalStyle`
+	@font-face {
+		font-family: "GreenHome";
+		src: url("/fonts/GreenHome.ttf");
+		font-style: normal;
+		font-weight: 400;
+	}
+	.my-main-font{
+		font-family: "GreenHome";
+	}
 `;
 
 export const MyContext = createContext(null);
@@ -24,7 +24,7 @@ function MyApp({ Component, pageProps }) {
 	const themeMode = theme === "light" ? lightTheme : darkTheme;
 	return (
 		<ThemeProvider theme={themeMode}>
-			<MyContext.Provider value={{themeToggler, getNowTheme}}>
+			<MyContext.Provider value={{ themeToggler, getNowTheme }}>
 				<FontStyles />
 				<Component {...pageProps} />
 				<StyledContainer />
