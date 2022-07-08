c9c613cde34785601a3004604f46bf6580a6be3e
xujianxiang <584972029@qq.com>
2022-07-08 13:30:54 +0800

    fix: add analytics

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 import { FiGithub } from "react-icons/fi";
 import { FaWeibo, FaFilePdf, FaTwitter } from "react-icons/fa";
+import { MdAnalytics } from "react-icons/md";
 import { BiPyramid } from "react-icons/bi";
 
 const SVG_SIZE = 22;
@@ -31,8 +32,11 @@ export const Footer = ({ showLink }) => {
 				<a href="https://juejin.cn/user/2330620383728551">
 					<BiPyramid width={SVG_SIZE} height={SVG_SIZE} />
 				</a>
-				<a href="/xujianxiang-resume.pdf">
+				<a href="https://umami-alfxjx.up.railway.app/share/eaSooCGG/Blog">
 					<FaFilePdf width={SVG_SIZE} height={SVG_SIZE} />
+				</a>
+				<a href="/xujianxiang-resume.pdf">
+					<MdAnalytics width={SVG_SIZE} height={SVG_SIZE} />
 				</a>
 			</IconList>
 		</FooterWrapper>
