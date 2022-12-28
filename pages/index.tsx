a3fa32f9f7d4bbf2058ac1e6687a27230711d753
alfxjx <xjxtju@163.com>
2022-12-28 09:03:55 +0000

    fix: grid layout css

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,41 @@ import React from "react"
 
 export default function Me() {
   return (
-    <div className="w-full min-h-screen overflow-hidden grid grid-cols-4 grid-rows-6 bg-white dark:bg-gray-800 text-black dark:text-gray-400">
-      <Card row={2} col={4}>01</Card>
-      <Card row={1} col={4}>02</Card>
-      <Card row={2} col={2}>03</Card>
+    <div className="p-2 w-5/6 min-h-screen mx-auto grid grid-cols-12 grid-flow-row-dense auto-rows-[128px] gap-2 bg-white dark:bg-gray-800 text-black dark:text-gray-400">
+      <div className="lg:col-span-6 lg:row-span-4">
+        <Card>01</Card>
+      </div>
+      <div className="lg:col-span-4 lg:row-span-3">
+        <Card>02</Card>
+      </div>
+      <div className="lg:col-span-2 lg:row-span-4">
+        <Card>03</Card>
+      </div>
+      <div className="lg:col-span-4 lg:row-span-1">
+        <Card>04</Card>
+      </div>
+      <div className="lg:col-span-3 lg:row-span-2">
+        <Card>05</Card>
+      </div>
+      <div className="lg:col-span-3 lg:row-span-2">
+        <Card>06</Card> 
+      </div>
+      <div className="lg:col-span-3 lg:row-span-2">
+        <Card>07</Card>
+      </div>
+      <div className="lg:col-span-3 lg:row-span-2">
+        <Card>08</Card>
+      </div>
     </div>
   )
 }
 
-interface ICardProps { row: number, col: number, children: React.ReactNode }
+interface ICardProps { children: React.ReactNode }
 
-const Card = ({ row, col, children }: ICardProps) => {
+const Card = ({ children }: ICardProps) => {
   return (
-    <div className={`col-span-${col} row-span-${row}`}>
-      <div className="w-full h-full rounded-lg bg-gray-200 dark:bg-gray-700">
-        {children}
-      </div>
+    <div className="w-full h-full rounded-lg bg-gray-200 dark:bg-gray-700">
+      {children}
     </div>
   )
 }
