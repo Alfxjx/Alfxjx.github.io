68b638539337d37d09e2e981e8417d44c2729ca7
alfxjx <xjxtju@163.com>
2023-01-03 07:34:40 +0000

    fix: layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,32 +9,34 @@ type Props = {
 
 export default function Me(_props: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className="p-2 w-5/6 min-h-screen mx-auto grid grid-cols-12 grid-flow-row-dense auto-rows-[15.5vh] gap-2 bg-white dark:bg-gray-800 text-black dark:text-gray-400">
-      <div className="lg:col-span-6 lg:row-span-4">
-        <Card>
-          <AboutMe></AboutMe>
-        </Card>
-      </div>
-      <div className="lg:col-span-4 lg:row-span-3">
-        <Card>02</Card>
-      </div>
-      <div className="lg:col-span-2 lg:row-span-4">
-        <Card>03</Card>
-      </div>
-      <div className="lg:col-span-4 lg:row-span-1">
-        <Card>04</Card>
-      </div>
-      <div className="lg:col-span-3 lg:row-span-2">
-        <Card>05</Card>
-      </div>
-      <div className="lg:col-span-3 lg:row-span-2">
-        <Card>06</Card>
-      </div>
-      <div className="lg:col-span-3 lg:row-span-2">
-        <Card>07</Card>
-      </div>
-      <div className="lg:col-span-3 lg:row-span-2">
-        <Card>08</Card>
+    <div className="w-full min-h-screen bg-white dark:bg-gray-800 text-black dark:text-gray-400">
+      <div className="p-2 w-5/6 min-h-screen mx-auto grid grid-cols-12 grid-flow-row-dense auto-rows-[15.2vh] gap-2 ">
+        <div className="lg:col-span-6 lg:row-span-4 sm:col-span-12 md:row-span-4">
+          <Card>
+            <AboutMe></AboutMe>
+          </Card>
+        </div>
+        <div className="lg:col-span-4 lg:row-span-3">
+          <Card>02</Card>
+        </div>
+        <div className="lg:col-span-2 lg:row-span-4">
+          <Card>03</Card>
+        </div>
+        <div className="lg:col-span-4 lg:row-span-1">
+          <Card>04</Card>
+        </div>
+        <div className="lg:col-span-3 lg:row-span-2">
+          <Card>05</Card>
+        </div>
+        <div className="lg:col-span-3 lg:row-span-2">
+          <Card>06</Card>
+        </div>
+        <div className="lg:col-span-3 lg:row-span-2">
+          <Card>07</Card>
+        </div>
+        <div className="lg:col-span-3 lg:row-span-2">
+          <Card>08</Card>
+        </div>
       </div>
     </div>
   )
