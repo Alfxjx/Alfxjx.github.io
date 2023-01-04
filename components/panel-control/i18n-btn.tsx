4c04daa6f72b2eedec22200f7425990f1a122dc7
alfxjx <xjxtju@163.com>
2023-01-04 13:52:44 +0000

    fix update i18n defaultlocale

--- components/panel-control/i18n-btn.tsx
+++ components/panel-control/i18n-btn.tsx
@@ -4,13 +4,13 @@ import React, { useMemo } from "react";
 import { HiLanguage } from "react-icons/hi2";
 
 export function I18nBtn() {
-	const { locale } = useRouter();
+	const { locale, defaultLocale } = useRouter();
 	const nextLocale = useMemo(() => (locale === "zh" ? "en" : "zh"), [locale]);
 	return (
 		<Link href={"/"} locale={nextLocale}>
 			<button className="btn btn-primary flex justify-center items-center">
 				<HiLanguage className="w-6 h-6"></HiLanguage>
-				<span className="ml-2">({locale})</span>
+				<span className="ml-2">({locale ?? defaultLocale})</span>
 			</button>
 		</Link>
 	);
